d7752ac901324a50bc1fa421ef585f7370dc15bc
Wallison Martins <wallisonpinheiro1@gmail.com>
2020-01-15 23:00:59 -0300

    versão 1.0 finalizada (dark-mode)

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <div className="headDevs">
+      <p>Find_Devs</p>
+    </div>
+  );
+}
+
+
+export default Header;
